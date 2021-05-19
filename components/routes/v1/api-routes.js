e0001c16253dae845de12a6a56c3ec54e8cff7f6
elizabethLomb <elizabeth.lombardi@gmail.com>
2021-05-19 17:00:40 +0200

    feat: some router

--- components/routes/v1/api-routes.js
+++ components/routes/v1/api-routes.js
@@ -1,8 +1,8 @@
 module.exports = () => {
 	const start = async ({ app, logger, controller }) => {
-		app.get('/', (req, res, next) => {
+		app.get('/v1/', (req, res, next) => {
 			controller.v1.api.getHome(res)
-				.then(r => res.status(200).json({ message: 'Getting home' }))
+				.then(r => r.status(200).json({ message: 'Getting home' }))
 				.catch(next)
 		})
 
